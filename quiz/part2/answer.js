2e0b80b6f02cf2c68df1c71ff4c085412a73aead
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-02-27 08:18:59 +0700

    mengerjakan quiz part 2

---
+++ quiz/part2/answer.js
@@ -0,0 +1,60 @@
+// console.log("soal ke-1");
+// let word = "Javascript";
+// let second = "is";
+// let third = "awesome";
+// let fourth = "and";
+// let fifth = "I";
+// let sixth = "love";
+// let seventh = "it!";
+
+// console.log(`${word} ${second} ${third} ${fourth} ${fifth} ${sixth} ${seventh}`);
+
+// console.log("soal ke-2");
+// let word2 = 'wow javascript is so cool';
+// let firstWord = word2[0] + word2[1] + word2[2];
+// let secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13];
+// let thirdWord = word2[15] + word2[16];
+// let fourthWord = word2[18] + word2[19];
+// let fifthWord = word2[21] + word2[22] + word2[23] + word2[24];
+// console.log(`${firstWord} ${secondWord} ${thirdWord} ${fourthWord} ${fifthWord}`)
+
+// console.log("soal ke-3")
+// let word3 = 'wow JavaScript is so cool';
+// let firstWord3 = word3.substring(0, 3);
+// let secondWord3 = word3.substring(4, 14);
+// let thirdWord3 = word3.substring(15, 17);
+// let fourthWord3 = word3.substring(18, 20);
+// let fifthWord3 = word3.substring(21, 25);
+
+// console.log('First Word: ' + firstWord3);
+// console.log("Second Word: " + secondWord3);
+// console.log("Third Word: " + thirdWord3);
+// console.log("Fourth Word: " + fourthWord3);
+// console.log("Fifth Word: " + fifthWord3);
+
+console.log("soal ke-4")
+let word4 = "Wow Javascript Is So Cool";
+let firstWord4 = word4.substring(0, 3);
+let firstWordLength = firstWord4.length;
+
+let secondWord4 = word4.substring(4, 14);
+let secondWordLength = secondWord4.length;
+
+let thirdWord4 = word4.substring(15, 17);
+let thirdWordLength = thirdWord4.length;
+
+let fourthWord4 = word4.substring(18, 20);
+let fourthWordLength = fourthWord4.length;
+
+let fifthWord4 = word4.substring(21, 25);
+let fifthWordLength = fifthWord4.length;
+
+
+console.log(`First Word: ${firstWord4}, With Length: ${firstWordLength}`);
+console.log(`Second Word: ${secondWord4}, With Length: ${secondWordLength}`);
+console.log(`Third Word: ${thirdWord4}, With Length: ${thirdWordLength}`);
+console.log(`Fourth Word: ${fourthWord4}, With Length: ${fourthWordLength}`);
+console.log(`Fifth Word: ${fifthWord4}, With Length: ${fifthWordLength}`);
+
+
+
